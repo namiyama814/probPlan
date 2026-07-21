a7c7f0b363a84b55c7e4166bf6654de2382c8c2f
namiyama-814 <namiyama814@gmail.com>
2026-07-21 22:01:39 +0900

    最低限のコードを記述

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const app = document.getElementById("app");
+
+app.innerHTML = `
+<div class="max-w-6xl mx-auto px-6 py-8">
+
+    <h1 class="text-4xl font-bold text-slate-800">
+        ProbPlan
+    </h1>
+
+    <p class="mt-2 text-slate-600">
+        ProbPlanは、計画を立てるためのツールです。
+    </p>
+
+</div>
+`;
