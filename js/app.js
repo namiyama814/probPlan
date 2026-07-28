36270b2396070eef33f0e46f2a5db9af62e955eb
namiyama <namiyama814@gmail.com>
2026-07-28 16:17:28 +0900

    ファイル名の大文字小文字を修正

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { ProjectManager } from "./models/ProjectManager.js";
+import { ProjectManager } from "./models/projectManager.js";
 import { StorageService } from "./services/storageService.js";
 import { createProject } from "./controllers/projectController.js";
 import { renderProjectSection } from "./ui/projectView.js";
